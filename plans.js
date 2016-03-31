4f17b1ca7e53a5b7ecf6e4b61ecdd5a51a479f67
alysiasaquil <alysiasaquil@gmail.com>
2016-03-31 14:07:03 -0700

    I created a home page after user has logged in, and created a js file for different types of fitness plans. #4

---
+++ plans.js
@@ -0,0 +1,47 @@
+var workoutPlan = [
+  {
+    plan: "Get Moving",
+    totalWeeks: "12 Weeks",
+    schedule: [
+      {
+        sunday: "REST",
+        monday: "Core Workout",
+        tuesday: "20-30 min cardio",
+        wednesday: "Upper Body Workout",
+        thursday: "20-30 min cardio",
+        friday: "Total Body Workout",
+        saturday: "20-30 min cardio"
+      }
+    ],
+  };
+  {
+    plan: "Build Muscle",
+    totalWeeks: "12 Weeks",
+    schedule: [
+      {
+        sunday: "REST",
+        monday: "Chest and Triceps",
+        tuesday: "Back and Biceps",
+        wednesday: "1 Hour Cardio",
+        thursday: "Shoulders and Forearms",
+        friday: "Legs",
+        saturday: "Rest"
+      }
+    ],
+  };
+  {
+    plan: "Lose Weight",
+    totalWeeks: "12 Weeks",
+    schedule: [
+      {
+        sunday: "REST",
+        monday: "Core Workout",
+        tuesday: "30-40 min cardio",
+        wednesday: "Upper Body Workout",
+        thursday: "30-40 min cardio",
+        friday: "Total Body Workout",
+        saturday: "50-60 min cardio"
+      }
+    ]
+  }
+];
