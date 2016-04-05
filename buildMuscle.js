f329221214712e1148d7ed8520338885081b71c3
alysiasaquil <alysiasaquil@gmail.com>
2016-04-05 09:38:32 -0700

    I commented out the code of two plans because the ability to create a plan won't work when all the codes are in.

--- buildMuscle.js
+++ buildMuscle.js
@@ -1,49 +1,49 @@
-var getPlanOne = document.getElementById('panelOneButton');
-var getPlanTwo = document.getElementById('panelTwoButton');
-var getPlanThree = document.getElementById('panelThreeButton');
-var getMoving = document.getElementById('panelOne');
-var buildMuscle = document.getElementById('panelTwo');
-var loseWeight = document.getElementById('panelThree');
-var createButton = document.getElementById('create');
-var planTypes = document.getElementById('planTypes');
-var chooseMsg = document.getElementById('chooseMsg');
-var nextIntro = document.getElementsByClassName('page2')[1];
-var nextLevel = document.getElementsByClassName('page3')[1];
-var nextGoals = document.getElementsByClassName('page4')[1];
-var generatePlan = document.getElementsByClassName('page5')[1];
-var submitPlan = document.getElementsByClassName('submitPlan')[1];
-var intro = document.getElementsByClassName('intro')[1];
-var level = document.getElementsByClassName('level')[1];
-var goal = document.getElementsByClassName('goal')[1];
-var days = document.getElementsByClassName('days')[1];
-var summary = document.getElementsByClassName('summary')[1];
-
-getPlanTwo.addEventListener('click', function() {
-  var createBuild = document.getElementById('buildMuscle');
-
-  chooseMsg.className = "hide";
-  getMoving.className = "hide";
-  createBuild.className = "show";
-  buildMuscle.className = "hide";
-  loseWeight.className = "hide";
-});
-
-nextIntro.addEventListener('click', function() {
-  intro.className = "hide";
-  level.className = "show";
-})
-
-nextLevel.addEventListener('click', function() {
-  level.className = "hide";
-  goal.className = "show";
-})
-
-nextGoals.addEventListener('click', function() {
-  goal.className = "hide";
-  days.className = "show";
-})
-
-generatePlan.addEventListener('click', function() {
-  days.className = "hide";
-  summary.className = "show";
-})
+// var getPlanOne = document.getElementById('panelOneButton');
+// var getPlanTwo = document.getElementById('panelTwoButton');
+// var getPlanThree = document.getElementById('panelThreeButton');
+// var getMoving = document.getElementById('panelOne');
+// var buildMuscle = document.getElementById('panelTwo');
+// var loseWeight = document.getElementById('panelThree');
+// var createButton = document.getElementById('create');
+// var planTypes = document.getElementById('planTypes');
+// var chooseMsg = document.getElementById('chooseMsg');
+// var nextIntro = document.getElementsByClassName('page2')[1];
+// var nextLevel = document.getElementsByClassName('page3')[1];
+// var nextGoals = document.getElementsByClassName('page4')[1];
+// var generatePlan = document.getElementsByClassName('page5')[1];
+// var submitPlan = document.getElementsByClassName('submitPlan')[1];
+// var intro = document.getElementsByClassName('intro')[1];
+// var level = document.getElementsByClassName('level')[1];
+// var goal = document.getElementsByClassName('goal')[1];
+// var days = document.getElementsByClassName('days')[1];
+// var summary = document.getElementsByClassName('summary')[1];
+//
+// getPlanTwo.addEventListener('click', function() {
+//   var createBuild = document.getElementById('buildMuscle');
+//
+//   chooseMsg.className = "hide";
+//   getMoving.className = "hide";
+//   createBuild.className = "show";
+//   buildMuscle.className = "hide";
+//   loseWeight.className = "hide";
+// });
+//
+// nextIntro.addEventListener('click', function() {
+//   intro.className = "hide";
+//   level.className = "show";
+// })
+//
+// nextLevel.addEventListener('click', function() {
+//   level.className = "hide";
+//   goal.className = "show";
+// })
+//
+// nextGoals.addEventListener('click', function() {
+//   goal.className = "hide";
+//   days.className = "show";
+// })
+//
+// generatePlan.addEventListener('click', function() {
+//   days.className = "hide";
+//   summary.className = "show";
+// })
